96188cbab9d400381889ff6317b063ca46d6ce19
jeffjeffjeffh <121068607+jeffjeffjeffh@users.noreply.github.com>
2023-01-08 20:37:42 -0800

    getTotalNumberOfBorrows completed

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,8 +1,9 @@
-//This function passed in Qualified, done
+// Passing
 function findAccountById(accounts, id) {
   return accounts.find((account) => account.id == id);
 }
 
+// Passing
 function sortAccountsByLastName(accounts) {
   const arrSorted = accounts.sort((a, b) => {
     return a.name.last < b.name.last ? -1 : 1;
@@ -10,7 +11,16 @@ function sortAccountsByLastName(accounts) {
   return arrSorted;
 }
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows({ id }, books) {
+  let total = 0;
+  books.forEach((book) => {
+    const arr = book.borrows;
+    arr.forEach((borrow) => {
+      if (borrow.id == id) total++;
+    });
+  });
+  return total;
+}
 
 function getBooksPossessedByAccount(account, books, authors) {}
 
