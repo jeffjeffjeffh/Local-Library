581e0cae70581fa1078da4e671c65cae2ea1602e
jeffjeffjeffh <121068607+jeffjeffjeffh@users.noreply.github.com>
2023-01-08 20:14:32 -0800

    accountsbylastname

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,8 +1,12 @@
+//This function passed in Qualified, done
 function findAccountById(accounts, id) {
-  return accounts.find(id);
+  return accounts.find((account) => account.id == id);
 }
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  const arrSorted = accounts.sort((a, b) => a.name.last - b.name.last);
+  return arrSorted;
+}
 
 function getTotalNumberOfBorrows(account, books) {}
 
