4ddc49e251ba0828cb664b0a554bd907a5813c34
Jeff Harm <jeffjeffjeffh@yahoo.com>
2023-01-09 14:31:30 -0800

    fixing it fixing it

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -11,6 +11,7 @@ function sortAccountsByLastName(accounts) {
   return arrSorted;
 }
 
+// Passing
 function getTotalNumberOfBorrows({ id }, books) {
   let total = 0;
   books.forEach((book) => {
@@ -22,7 +23,52 @@ function getTotalNumberOfBorrows({ id }, books) {
   return total;
 }
 
-function getBooksPossessedByAccount(account, books, authors) {}
+// Passing
+function getBooksPossessedByAccount({ id }, books, authors) {
+  const borrowedBooks = getBorrowedBooks(id, books);
+  const borrowedAuthors = getAuthorObjects(borrowedBooks, authors);
+  const newBooks = injectAuthors(borrowedBooks, borrowedAuthors);
+  console.log(newBooks);
+  return newBooks;
+}
+
+// Helper function that returns an array containing all of the book
+// objects that an individual is CURRENTLY borrowing.
+const getBorrowedBooks = (userId, books) => {
+  let arr = books.filter((book) => {
+    if (book.borrows[0].id == userId && !book.borrows[0].returned) {
+      return book;
+    }
+  });
+  return arr;
+};
+
+// Helper function that returns an array of objects
+// for every author represented in a list of books
+const getAuthorObjects = (books, authors) => {
+  const arr = authors.filter((author) => {
+    for (let key in books) {
+      let book = books[key];
+      if (book.authorId == author.id) return true;
+    }
+  });
+  return arr;
+};
+
+// Helper function that adds author information
+// to an array of book objects
+const injectAuthors = (books, authors) => {
+  let arr = books.map((book) => {
+    for (let key in authors) {
+      let author = authors[key];
+      if (author.id == book.authorId) {
+        book.author = author;
+        return book;
+      }
+    }
+  });
+  return arr;
+};
 
 module.exports = {
   findAccountById,
