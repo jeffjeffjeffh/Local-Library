351d6df2ee38b5fbf2bcfe15d6bc3bb087c03b08
Jeff Harm <jeffjeffjeffh@yahoo.com>
2023-01-10 12:00:25 -0800

    Complete!

--- public/src/home.js
+++ public/src/home.js
@@ -26,6 +26,7 @@ function getMostCommonGenres(books) {
     Look for a genre match for each book. If a match is not found, add one to
     the array and give it a count of 1. Otherwise, retrieve that genre's index
     in the array and increase its count property by 1.
+    Might refactor to use map instead of WHATEVER this is.
   */
   books.forEach((book) => {
     if (!arr.find((genre) => genre.name == book.genre)) {
@@ -40,6 +41,7 @@ function getMostCommonGenres(books) {
   return getHighest(arr, 5);
 }
 
+// Passing
 function getMostPopularBooks(books) {
   let bestBooks = books.map((book) => {
     return { name: book.title, count: book.borrows.length };
@@ -47,7 +49,27 @@ function getMostPopularBooks(books) {
   return getHighest(bestBooks, 5);
 }
 
-function getMostPopularAuthors(books, authors) {}
+// Passing
+function getMostPopularAuthors(books, authors) {
+  /*
+    Map the authors array to a new one by matching their id property to each book.
+    Add up the total borrows for that author's matching books.
+    Then pass a new object containing the author's formatted name and count to back to the map.
+  */
+  let bestAuthors = authors.map((author) => {
+    let bookCount = books.reduce((total, book) => {
+      if (book.authorId == author.id) {
+        return total + book.borrows.length;
+      }
+      return total;
+    }, 0);
+    return {
+      name: author.name.first + " " + author.name.last,
+      count: bookCount,
+    };
+  });
+  return getHighest(bestAuthors, 5);
+}
 
 // Helper function that sorts an array by its objects' 'count' property,
 // in descending order, and returns the top x.
