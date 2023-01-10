1128752be9d710f2172474f9d10ef2919d94ff60
Jeff Harm <jeffjeffjeffh@yahoo.com>
2023-01-10 12:07:17 -0800

    I don't know what happened

--- test/myTest.js
+++ test/myTest.js
@@ -1,6 +1,7 @@
 const books = require("./fixtures/books.fixture");
+const authors = require("./fixtures/authors.fixture");
 
-function getMostCommonGenres(books) {
+/*function getMostCommonGenres(books) {
   arr = [];
   books.forEach((book) => {
     if (!arr.find((genre) => genre.name == book.genre)) {
@@ -17,3 +18,26 @@ function getMostCommonGenres(books) {
 }
 
 console.log(getMostCommonGenres(books));
+*/
+
+function getMostPopularAuthors(books, authors) {
+  let bestAuthors = authors.map((author) => {
+    let bookCount = books.reduce((total, book) => {
+      if (book.authorId == author.id) {
+        return total + book.borrows.length;
+      }
+      return total;
+    }, 0);
+    return {
+      name: author.name.first + " " + author.name.last,
+      count: bookCount,
+    };
+  });
+  return getHighest(bestAuthors, 5);
+}
+
+function getHighest(arr, count) {
+  return arr.sort((a, b) => b.count - a.count).slice(0, count);
+}
+
+console.log(getMostPopularAuthors(books, authors));
