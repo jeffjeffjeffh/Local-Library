d44481d9522830d394849aab97a9501cf3ed6f7a
Jeff Harm <jeffjeffjeffh@yahoo.com>
2023-01-09 16:26:30 -0800

    getBorrowersForBook done

--- public/src/books.js
+++ public/src/books.js
@@ -3,17 +3,28 @@ function findAuthorById(authors, id) {
   return authors.find((author) => id == author.id);
 }
 
+// Passing
 function findBookById(books, id) {
   return books.find((book) => id == book.id);
 }
 
+// Passing
 function partitionBooksByBorrowedStatus(books) {
   let borrowedBooks = books.filter((book) => !book.borrows[0].returned);
   let unborrowedBooks = books.filter((book) => book.borrows[0].returned);
   return [borrowedBooks, unborrowedBooks];
 }
 
-function getBorrowersForBook(book, accounts) {}
+// Passing
+function getBorrowersForBook({ borrows }, accounts) {
+  let result = borrows.map((borrow) => {
+    const accountInfo = accounts.find((account) => account.id == borrow.id);
+    const newBorrow = { ...borrow, ...accountInfo };
+    return newBorrow;
+  });
+  result.splice(10);
+  return result;
+}
 
 module.exports = {
   findAuthorById,
